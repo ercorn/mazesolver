1ee5d42896f49e6bc5e42537fe3303c61728bbc2
rcorn013 <rctexvn@gmail.com>
2025-02-20 18:16:51 -0500

    minor changes to maze for naming consistency

--- cell.py
+++ cell.py
@@ -2,40 +2,42 @@ from graphics import Line, Point
 
 class Cell:
     def __init__(self, win):
-        self.__x1 = None
-        self.__x2 = None
-        self.__y2 = None
-        self.__y1 = None
+        self._x1 = None
+        self._x2 = None
+        self._y2 = None
+        self._y1 = None
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.__win = win
+        self._win = win
 
     def draw(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__x2 = x2
-        self.__y1 = y1
-        self.__y2 = y2
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
-            self.__win.draw_line(line)
+            self._win.draw_line(line)
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
-            self.__win.draw_line(line)
+            self._win.draw_line(line)
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
-            self.__win.draw_line(line)
+            self._win.draw_line(line)
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
-            self.__win.draw_line(line)
+            self._win.draw_line(line)
 
     def draw_move(self, to_cell, undo=False):
-        fill_color = "gray"
+        fill_color = "red"
         if undo:
-            fill_color = "red"
+            fill_color = "gray"
         line = Line(
-                    Point((self.__x1 + self.__x2)/2, (self.__y1 + self.__y2)/2), 
-                    Point((to_cell.__x1 + to_cell.__x2)/2, (to_cell.__y1 + to_cell.__y2)/2)
+                    Point((self._x1 + self._x2)/2, (self._y1 + self._y2)/2), 
+                    Point((to_cell._x1 + to_cell._x2)/2, (to_cell._y1 + to_cell._y2)/2)
                     )
-        self.__win.draw_line(line, fill_color)
+        self._win.draw_line(line, fill_color)
