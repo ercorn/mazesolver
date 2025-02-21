1a19f03761667a1c4331c22b7184e7477d2d6230
rcorn013 <rctexvn@gmail.com>
2025-02-20 23:38:36 -0500

    added break entrance exit test and fixed bug

--- maze.py
+++ maze.py
@@ -42,5 +42,5 @@ class Maze:
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
         self._draw_cell(0, 0)
-        self._cells[self._num_rows - 1][self._num_cols - 1].has_bottom_wall = False
-        self._draw_cell(self._num_rows - 1, self._num_cols - 1)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
