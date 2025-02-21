714f88d2f56aaa9370dc7d91ff36172172f5ebd6
rcorn013 <rctexvn@gmail.com>
2025-02-21 17:12:55 -0500

    add break_walls_r func to generate maze

--- maze.py
+++ maze.py
@@ -1,8 +1,9 @@
 from cell import Cell
 import time
+import random
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None):
         self._x1 = x1
         self._y1 = y1
         self._num_rows = num_rows
@@ -12,8 +13,12 @@ class Maze:
         self._win = win
         self._cells = []
 
+        if seed:
+            random.seed(seed)
+
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
 
     def _animate(self):
         self._win.redraw()
@@ -31,11 +36,6 @@ class Maze:
 
     def _create_cells(self):
         self._cells = [[Cell(self._win) for _ in range(self._num_rows)] for _ in range(self._num_cols)]
-        #for i in range(self._num_cols):
-        #    col_cells = []
-        #    for j in range(self._num_rows):
-        #        col_cells.append(Cell(self._win))
-        #    self._cells.append(col_cells)
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._draw_cell(i, j)
@@ -45,3 +45,47 @@ class Maze:
         self._draw_cell(0, 0)
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+            #Check the cells that are directly adjacent to the current cell. 
+            # Keep track of any that have not been visited as "possible directions" to move to
+            if i > 0 and not self._cells[i - 1][j].visited: #right side: is not leftmost and more exist rightward
+                to_visit.append((i - 1, j))
+            if i < self._num_cols - 1 and not self._cells[i + 1][j].visited: #left side: is not rightmost and more exist leftward
+                to_visit.append((i + 1, j))
+            if j > 0 and not self._cells[i][j - 1].visited: #upwards: is not topmost and more exist downward
+                to_visit.append((i, j - 1))
+            if j < self._num_rows - 1 and not self._cells[i][j + 1].visited: #downwards: is not downmost and more exist upward
+                to_visit.append((i, j + 1))
+
+            #If there are zero directions you can go from the current cell, then draw the current cell and return
+            if len(to_visit) == 0:
+                self._draw_cell(i, j)
+                return
+            
+            #Otherwise, pick a random direction
+            next_dir_idx = to_visit[random.randrange(len(to_visit))]
+
+            #Knock down the walls between the current cell and the chosen cell
+            #left
+            if next_dir_idx[0] == i - 1:
+                self._cells[i][j].has_left_wall = False
+                self._cells[i - 1][j].has_right_wall = False            
+            #right
+            if next_dir_idx[0] == i + 1:
+                self._cells[i][j].has_right_wall = False
+                self._cells[i + 1][j].has_left_wall = False
+            #up
+            if next_dir_idx[1] == j - 1:
+                self._cells[i][j].has_top_wall = False
+                self._cells[i][j - 1].has_bottom_wall = False            
+            #down
+            if next_dir_idx[1] == j + 1:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i][j + 1].has_top_wall = False
+
+            #Move to the chosen cell by recursively calling _break_walls_r
+            self._break_walls_r(next_dir_idx[0], next_dir_idx[1])
