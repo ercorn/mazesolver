c55a4bb758be56f4c64a702cf6684c5dc06beffa
rcorn013 <rctexvn@gmail.com>
2025-02-20 22:25:26 -0500

    corrected out of bounds bug  create_cells in maze

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from graphics import Window, Point, Line
 from cell import Cell
+from maze import Maze
 
 def main():
-    win = Window(800, 600)
+    #win = Window(800, 600)
 
-    a = Cell(win)
-    a.draw(300, 300, 500, 500)
+    #a = Cell(win)
+    #a.draw(300, 300, 500, 500)
 
-    b = Cell(win)
-    b.draw(150, 150, 100, 100)
+    #b = Cell(win)
+    #b.draw(150, 150, 100, 100)
 
-    a.draw_move(b)
+    #a.draw_move(b)
+
+    #test_maze = Maze(0, 0, 5, 5, 50, 50, win)
+
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
     win.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -12,20 +12,29 @@ class Maze:
         self._win = win
         self._cells = []
 
+        self._create_cells()
+
     def _animate(self):
-        self.__win.redraw()
+        self._win.redraw()
         time.sleep(0.05)
 
     def _draw_cell(self, i, j):
-        cell_x1 = self._x1 + (self._cell_size_x * j)
-        cell_y1 = self._y1 + (self._cell_size_y * i)
+        if self._win is None:
+            return
+        cell_x1 = self._x1 + (self._cell_size_x * i)
+        cell_y1 = self._y1 + (self._cell_size_y * j)
         cell_x2 = cell_x1 + self._cell_size_x
         cell_y2 = cell_y1 + self._cell_size_y
         self._cells[i][j].draw(cell_x1, cell_y1, cell_x2, cell_y2)
         self._animate()
 
     def _create_cells(self):
-        self._cells = [[Cell(self._win) for _ in range(self._num_cols)] for _ in range(self._num_rows)]
+        self._cells = [[Cell(self._win) for _ in range(self._num_rows)] for _ in range(self._num_cols)]
+        #for i in range(self._num_cols):
+        #    col_cells = []
+        #    for j in range(self._num_rows):
+        #        col_cells.append(Cell(self._win))
+        #    self._cells.append(col_cells)
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._draw_cell(i, j)
