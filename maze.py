0da878182c4617b1649b90d96e8878f6af55641b
rcorn013 <rctexvn@gmail.com>
2025-02-20 23:26:33 -0500

    added break_entrance_and exit to maze

--- maze.py
+++ maze.py
@@ -38,3 +38,9 @@ class Maze:
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._draw_cell(i, j)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_rows - 1][self._num_cols - 1].has_bottom_wall = False
+        self._draw_cell(self._num_rows - 1, self._num_cols - 1)
